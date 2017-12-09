2704c5d6aa81313234daaef6cabecce1b24feee8
Chris <baederchris@gmail.com>
2017-12-09 23:19:19 +0100

    stuff works even bettor now, header and something

--- src/servlets/calculateServlet.java
+++ src/servlets/calculateServlet.java
@@ -12,46 +12,6 @@ import java.lang.Math;
 
 public class calculateServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        final String height = request.getParameter("height");
-        final String weight = request.getParameter("weight");
-        final String sex = request.getParameter("sex");
-        String resultMsg = "";
-        String contextPath = "";
-        
-        double heightCm = Double.parseDouble(height);
-        double weightKg = Double.parseDouble(weight);
-        double bmi = weightKg / (Math.pow(heightCm/100,2));
-        
-        if(bmi<16) {
-            resultMsg = "You are severly underweight";
-        } else if (bmi>=16 && bmi<17) {
-            resultMsg = "You are moderately underweight";
-        } else if (bmi>=17 && bmi<18.5) {
-            resultMsg = "You are slightly underweight";
-        } else if (bmi>=18.5 && bmi<25) {
-            resultMsg = "You are of normal weight";
-        } else if (bmi>=25 && bmi<30) {
-            resultMsg = "You are overweight";
-        } else if (bmi>=30 && bmi<35) {
-            resultMsg = "Adipositas I";
-        } else if (bmi>=35 && bmi<40) {
-            resultMsg = "Adipositas II";
-        } else if (bmi>=40) {
-            resultMsg = "Adipositas III";
-            response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
-            response.setHeader("Location", "adipositas1.jsp");
-        }
-        
-        //pack result of function into request attrs:
-        request.setAttribute("sex", sex);
-        request.setAttribute("height", heightCm);
-        request.setAttribute("weight", weightKg);
-        request.setAttribute("resultMsg", resultMsg);
-        
-        //render that biatch
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/resultsBmi.jsp");
-        rd.include(request, response);
-
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
