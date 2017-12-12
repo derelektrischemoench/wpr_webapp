ef208cda7dae481e3d8df4b187e0a210cc77ec5a
Chris <baederchris@gmail.com>
2017-12-12 21:26:43 +0100

    serialize with json save for now try with .ser files

--- src/models/User.java
+++ src/models/User.java
@@ -1,15 +1,21 @@
 package models;
+
 import resources.gson.*;
 
+import javax.swing.*;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class User {
     private String username;
     private String password;
+    private static final String filePath = new JFileChooser().getFileSystemView().getDefaultDirectory().toString();
     
     
-    public User() {}
+    public User() {
+    }
+    
     public User(String username, String password) {
         super();
         this.username = username;
@@ -20,17 +26,46 @@ public class User {
         //use gson to serialize ze user to json for persistence
         Gson gsonSerializer = new Gson();
         String jsonString = gsonSerializer.toJson(u);
-    
-        System.out.println(jsonString);
+        String initialJsonContent = "{\"users\": [\n" +
+                                            "  {\"username\": \"a\", \"password\": \"b\"},\n" +
+                                            "  {\"username\": \"c\", \"password\": \"d\"}\n" +
+                                            "]}";
+       
         
-        //dump that suckah to file yo
-        //TODO: note; to view stdout output start the server in debug mode and enjoy log values under the glassfish log section in "debug"
+        //create the serializedusers.json if it doesnt exist yet and prepopulate with the initial json structure
         try {
-            FileWriter fw = new FileWriter("serializedUsers.json");
+            File f = new File(filePath + "/IdeaProjects/wprLernplatform/src/models/serializedUsers.json");
+            
+            if (f.createNewFile()) {
+                //write the initial json structure:
+                
+                try {
+                    FileWriter fw = new FileWriter(filePath + "/IdeaProjects/wprLernplatform/src/models/serializedUsers.json", true);
+                    fw.write(initialJsonContent);
+                    fw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("File successfully created");
+            } else {
+                System.out.println("File already exists");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        
+        
+        //write the users to the json
+        try {
+            FileWriter fw = new FileWriter(filePath + "/IdeaProjects/wprLernplatform/src/models/serializedUsers.json", true);
             fw.write(jsonString);
             fw.close();
+            
         } catch (IOException e) {
-            System.out.println("fml");
+            e.printStackTrace();
         }
+        
+        
     }
 }
